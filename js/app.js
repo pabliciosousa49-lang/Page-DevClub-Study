5d535619b6c78fbaec0b2618ca54ac840bcb5b56
José Pablicio Sousa Viana <pabliciosousa49@gmail.com>
2026-09-13 02:22:35 -0300

    adição de seleção de elementos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
-// FASE 3 — Controlará o fluxo principal da aplicação.
-// Responsabilidades futuras:
-// - Observar elementos do DOM
-// - Escutar ações do usuário
-// - Controlar a troca entre etapas
-// - Organizar os dados do bloco
+// Etapas — Seleção
+
+const abrirBloco = document.getElementById("abrir-bloco");
+const inserirCodigo = document.getElementById("inserir-codigo");
+const analiseIa = document.getElementById("analise-ia");
+const quiz = document.getElementById("quiz");
+const finalizacao = document.getElementById("finalizacao");
+
+// Elementos: Abrir bloco — Seleção
+
+const tituloTrechoInput = document.getElementById("titulo-trecho");
+const linguagemSelect = document.getElementById("linguagem");
+const continuarBtn = document.getElementById("continuar-btn");
+
+// Elementos: Inserir código — Seleção
+
+const tituloTrechoExibicao = document.getElementById("titulo-trecho-exibicao");
+const linguagemExibicao = document.getElementById("linguagem-exibicao");
+const codigoInput = document.getElementById("codigo-input");
+const voltarInicioBtn = document.getElementById("voltar-inicio-btn");
+const analisarBtn = document.getElementById("analisar-btn");
+
+// Elementos: Análise — Seleção
+
+const voltarCodigoBtn = document.getElementById("voltar-codigo-btn");
+const gerarQuizBtn = document.getElementById("gerar-quiz-btn");
+
+// Elementos: Quiz — Seleção
+
+const progressoQuiz = document.getElementById("progresso-quiz");
+const perguntaAnteriorBtn = document.getElementById("pergunta-anterior-btn");
+const avancarPerguntaBtn = document.getElementById("avancar-pergunta-btn");
+
+// Elementos: Finalização —- Seleção
+
+const salvarBlocoBtn = document.getElementById("salvar-bloco-btn");
+const salvarPdfBtn = document.getElementById("salvar-pdf-btn");
