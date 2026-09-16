3c017726c09812a9b3b570548478e25787d81b8e
José Pablicio Sousa Viana <pabliciosousa49@gmail.com>
2026-09-16 18:51:12 -0300

    inicio e automatização de requesição- substituição do curl

--- js/api.js
+++ js/api.js
@@ -1,2 +1,18 @@
-// FASE 5 — Comunicação do frontend com o backend/API.
-// A chave da API NÃO deverá ficar exposta neste arquivo.
+// Análise pela API — Processamento - preparar comunicação do frontend com o backend
+async function analisarCodigo(titulo, linguagem, codigo) {
+
+    // Requisição ao backend — Processamento - enviar os dados do código para a rota de análise
+    const resposta = await fetch("http://localhost:3000/api/analisar", {
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            titulo,
+            linguagem,
+            codigo
+        })
+    });
+}
